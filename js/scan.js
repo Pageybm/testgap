6fae73103050ca336e3e68c3a8bbf1a8d3298b17
Pageybm <pageybm@gmail.com>
2014-07-12 11:33:54 -0400

    google again

--- js/scan.js
+++ js/scan.js
@@ -11,9 +11,8 @@ function startScan() {
 cordova.plugins.barcodeScanner.scan(
 function (result) {
 
-                              window.plugins.childBrowser.showWebPage('http://www.google.com',
-                                        { showLocationBar: true });
- 
+window.plugins.childBrowser.openExternal('http://www.google.com');
+
 //var s = "Result: " + result.text + "<br/>" +
 //"Format: " + result.format + "<br/>" +
 // "Cancelled: " + result.cancelled;
