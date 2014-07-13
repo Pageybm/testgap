782636417df195b4819af950e157f4796d00fa77
Pageybm <pageybm@gmail.com>
2014-07-12 21:01:27 -0400

    Dynamic URL inappbrowser

--- js/scan.js
+++ js/scan.js
@@ -1,6 +1,8 @@
 var resultDiv;
 
 document.addEventListener("deviceready", init, false);
+
+
 function init() {
 document.querySelector("#startScan").addEventListener("touchend", startScan, false);
 resultDiv = document.querySelector("#results");
@@ -11,6 +13,12 @@ function startScan() {
 cordova.plugins.barcodeScanner.scan(
 function (result) {
 
+        var ref = window.open(result.text, '_blank', 'location=yes');
+         ref.addEventListener('loadstart', function(event) { alert('start: ' + event.url); });
+         ref.addEventListener('loadstop', function(event) { alert('stop: ' + event.url); });
+         ref.addEventListener('loaderror', function(event) { alert('error: ' + event.message); });
+         ref.addEventListener('exit', function(event) { alert(event.type); });
+ 
 // window.plugins.childBrowser.openExternal('http://www.google.com');
 // this goes to a page window.location.replace("http://www.eventsnearhere.com/login");
 
